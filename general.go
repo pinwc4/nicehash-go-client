82335355c46de4d9820aeada1324a23c4f329e1a
pinwc4 <pinwc4@flipsidehobbies.com>
2023-05-11 14:17:14 -0500

    Begin adding mining algo get

--- general.go
+++ general.go
@@ -1,18 +1,60 @@
 package nhclient
 
+import (
+	"encoding/json"
+
+	"github.com/pkg/errors"
+)
+
 type general struct {
 	client *client
 }
 
-func (g *general) GetMiningAlgorithms() error {
-	g.client.doRequest(
+type miningAlgoList struct {
+	MiningAlgorithms []*miningAlgoInfo
+}
+
+type miningAlgoInfo struct {
+	Algorithm             string
+	Title                 string
+	Enabled               bool
+	Order                 int64
+	DisplayMiningFactor   string
+	MiningFactor          float64
+	DisplayMarketFactor   string
+	MarketFactor          float64
+	MinimalOrderAmount    float64
+	MinSpeedLimit         float64
+	MaxSpeedLimit         float64
+	PriceDownStep         float64
+	MinimalPoolDifficulty float64
+	Port                  int64
+	Color                 string
+	OrdersEnabled         bool
+	EnabledMarkets        string
+	DisplayPriceFactor    string
+	PriceFactor           float64
+}
+
+func (g *general) GetMiningAlgorithms() (algos *miningAlgoList, err error) {
+	responseBody, err := g.client.doRequest(
 		"GET",
 		"/main/api/v2/mining/algorithms",
 		nil,
 		nil,
 	)
 
-	return errNotImplemented
+	if err != nil {
+		return nil, errors.Wrap(err, "executing request")
+	}
+
+	err = json.Unmarshal(responseBody, &algos)
+	if err != nil {
+		return nil, errors.Wrap(err, "unmarshalling response body")
+	}
+
+	return algos, nil
+
 }
 
 func (g *general) GetMiningMarkets() error {
